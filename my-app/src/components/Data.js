7385096553476846f24562a45317913f3e56c2fc
FrancescoSciab <francesco.sciabbarrasii@gmail.com>
2023-06-05 12:11:42 +0100

    saving on localStorage

--- my-app/src/components/Data.js
+++ my-app/src/components/Data.js
@@ -1,10 +1,18 @@
 export default function Data(props) {
     const seconds = Math.floor(props.elapsedTime / 1000)
+
+    
+    localStorage.setItem("bestTime", seconds)
+    const bestTime = localStorage.getItem("bestTime")
+    
+    
+    
     return(
         <div>
             <p>You took {seconds} seconds</p>
 
-            <p>Best time: </p>
+            <p>Best time: {bestTime} seconds</p>
         </div>
+    
     )
 }
