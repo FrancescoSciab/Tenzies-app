febfcf37ec441b37e41e7d34bd21c4b243d3ffb4
FrancescoSciab <francesco.sciabbarrasii@gmail.com>
2023-06-10 12:36:29 +0100

    array not working

--- my-app/src/components/Data.js
+++ my-app/src/components/Data.js
@@ -1,19 +1,19 @@
 import { useEffect, useState } from "react"
 
 export default function Data(props) {
-    const seconds = Math.floor(props.elapsedTime / 1000)
-    const [bestTime, setBestTime] = useState(localStorage.getItem("bestTime"))
-    
-    console.log(bestTime)
-    
-    // useEffect(() => {
-    //     if(seconds < bestTime) {
-    //         localStorage.setItem("bestTime", seconds)
-    //         setBestTime(<p>Best Time: {localStorage.getItem("bestTime")}seconds</p>)
-    //     }
-    // }, [bestTime])
+    const seconds = Math.floor(props.elapsedTime / 1000);
+    const bestTime = [];
     
     
+    useEffect(() => {
+        
+        function loopOver() { 
+            bestTime.push(seconds)
+        }
+        const timeList = bestTime.map(loopOver)
+        
+        localStorage.setItem("TimeList", timeList)
+    }, [seconds])
 
     return(
         <div>
