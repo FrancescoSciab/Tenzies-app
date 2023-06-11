8c38b47e7b7991e52a92c88718ebb27f5905ec3b
FrancescoSciab <francesco.sciabbarrasii@gmail.com>
2023-06-11 17:07:55 +0100

    best TIme working

--- my-app/src/components/Data.js
+++ my-app/src/components/Data.js
@@ -1,32 +1,29 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export default function Data(props) {
     const seconds = Math.floor(props.elapsedTime / 1000);
-    const bestTime = []
-
-    function getHighestValue(number) {
-        return Math.max(number)
-    }
-
-    function saveTime() {
-        bestTime.push(seconds)
-        //loop over besttime array
-        const record = bestTime.forEach(getHighestValue)
-
-        localStorage.setItem("bestTime", record)
-    }
-
-    //as seconds are loaded, push them to localstorage
-    useEffect(() => {
-        saveTime()
-        localStorage.setItem("bestTime", bestTime)
-    })
+    const [bestTime, setBestTime] = useState(localStorage.getItem("bestTime"))
+    console.log(seconds)
     
+    useEffect(() => {
+        if(!bestTime) {
+            setBestTime(seconds)
+        }
+    }, [bestTime])
+        
+    
+    
+    useEffect(() => {
+        if(seconds <= bestTime) {
+            localStorage.setItem("bestTime", seconds)
+        }
+    }, [seconds])
+
 
     return(
         <div>
             <p>You took {seconds} seconds</p>
-            
+            <p>Best Time: {}</p>
         </div>
     
     )
