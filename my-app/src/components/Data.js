2f70d2056a7fce07425212d104c39b05ac433772
FrancescoSciab <francesco.sciabbarrasii@gmail.com>
2023-06-11 17:18:02 +0100

    feature working, to be tested

--- my-app/src/components/Data.js
+++ my-app/src/components/Data.js
@@ -2,9 +2,12 @@ import { useEffect, useState } from "react"
 
 export default function Data(props) {
     const seconds = Math.floor(props.elapsedTime / 1000);
+    //localstorage best time
     const [bestTime, setBestTime] = useState(localStorage.getItem("bestTime"))
+    const [record, setRecord] = useState(0)
     console.log(seconds)
     
+    //first time users will have localstorage set up
     useEffect(() => {
         if(!bestTime) {
             setBestTime(seconds)
@@ -12,18 +15,23 @@ export default function Data(props) {
     }, [bestTime])
         
     
-    
+    //to check shortest elapsed time before setting localstorage
     useEffect(() => {
         if(seconds <= bestTime) {
             localStorage.setItem("bestTime", seconds)
         }
     }, [seconds])
 
+    
+    useEffect(() => {
+        setRecord(localStorage.getItem("bestTime"))
+    }, [record])
+
 
     return(
         <div>
             <p>You took {seconds} seconds</p>
-            <p>Best Time: {}</p>
+            <p>Best Time: {record}</p>
         </div>
     
     )
