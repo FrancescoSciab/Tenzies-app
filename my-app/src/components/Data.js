8f78427b4c0883edc92e4f885a1b0d83938a318b
FrancescoSciab <francesco.sciabbarrasii@gmail.com>
2023-06-11 16:00:58 +0100

    with array not working

--- my-app/src/components/Data.js
+++ my-app/src/components/Data.js
@@ -1,24 +1,32 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 
 export default function Data(props) {
     const seconds = Math.floor(props.elapsedTime / 1000);
-    const bestTime = [];
-    
-    
+    const bestTime = []
+
+    function getHighestValue(number) {
+        return Math.max(number)
+    }
+
+    function saveTime() {
+        bestTime.push(seconds)
+        //loop over besttime array
+        const record = bestTime.forEach(getHighestValue)
+
+        localStorage.setItem("bestTime", record)
+    }
+
+    //as seconds are loaded, push them to localstorage
     useEffect(() => {
-        
-        function loopOver() { 
-            bestTime.push(seconds)
-        }
-        const timeList = bestTime.map(loopOver)
-        
-        localStorage.setItem("TimeList", timeList)
-    }, [seconds])
+        saveTime()
+        localStorage.setItem("bestTime", bestTime)
+    })
+    
 
     return(
         <div>
             <p>You took {seconds} seconds</p>
-            {bestTime}
+            
         </div>
     
     )
